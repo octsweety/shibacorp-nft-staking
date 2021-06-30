5701c1ddc818fc215d544edfb7a740ea831934b4
sagheerali1008 <alisahheer1008@gmail.com>
2021-06-30 21:44:31 +0400

    Update bshiba staking mvp version

--- address.ts
+++ address.ts
@@ -1,15 +1,19 @@
 export const address = {
     mainnet: {
         bshiba: "0x6158b3435DC3bc54a19A32Da2A2ed22aeC3bEF3e",
-        nftFactory: "0x7Ca29F0DB5Db8b88B332Aa1d67a2e89DfeC85E7E",
+        nftFactory: "0x376a60EBc4aDD3cceC5230461257aA975c5B9552",
+        nft: "0xF3fD8DDfFd06ccD9473848745ee60ac0FFe54a61",
         poolFactory: "0x7Ca29F0DB5Db8b88B332Aa1d67a2e89DfeC85E7E",
-        bshibaPool: "0x7Ca29F0DB5Db8b88B332Aa1d67a2e89DfeC85E7E"
+        bshibaPool: "0x7Ca29F0DB5Db8b88B332Aa1d67a2e89DfeC85E7E",
+        bshibaPoolMVP: "0x1b1dDe68cBeCeC7913eb5B738fbE8Ed310e50ef3"
     },
     testnet: {
         bshiba: "0x32006ce1b3A65b965cfc4f83403C2AA18c73d400",
         nftFactory: "0x24029a681d971b91A685a967259cf1Db45f05474",
+        nft: "0x537c17d0688c25BE1C0C4E795eE26fC8f432542f",
         poolFactory: "0x7Ca29F0DB5Db8b88B332Aa1d67a2e89DfeC85E7E",
-        bshibaPool: "0x44BCd20d278004BC476fB29293B9A9a2Ef1C8dc3"
+        bshibaPool: "0x44BCd20d278004BC476fB29293B9A9a2Ef1C8dc3",
+        bshibaPoolMVP: "0xB2FD390d8C5E2d8bCc55074b55a6Abe034313e5F"
     }
 }
 
